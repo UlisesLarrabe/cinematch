be39b42af2810a2168be2998c54638a04c52894d
uliseslarrabe <larrabeulises@gmail.com>
2026-03-20 03:33:09 -0300

    feat: add SVG components for CirclePlus, HandFinger, Movie, and Share

---
+++ src/svgs/share.tsx
@@ -0,0 +1,24 @@
+const Share = () => {
+  return (
+    <svg
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M6 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+      <path d="M18 6m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+      <path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+      <path d="M8.7 10.7l6.6 -3.4" />
+      <path d="M8.7 13.3l6.6 3.4" />
+    </svg>
+  );
+};
+
+export default Share;

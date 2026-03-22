2ae8ddd069558d0a7cb287128bf70a4630b6b3ad
uliseslarrabe <larrabeulises@gmail.com>
2026-03-22 14:58:26 -0300

    fix: replace MoreInfoSection with HowItWorks component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Hero from "@/components/hero";
-import MoreInfoSection from "@/components/more-info-section";
+import HowItWorks from "@/components/how-it-works";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen bg-neutral justify-center items-center w-full">
       <Hero />
-      <MoreInfoSection />
+      <HowItWorks />
     </main>
   );
 }
